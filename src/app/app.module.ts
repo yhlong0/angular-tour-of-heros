6931da82c6349e42e6a541ce13daff8a1cfa6ff2
hailong <yhlong2@gmail.com>
2018-09-21 10:03:54 -0500

    add two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
@@ -10,7 +11,8 @@ import { HerosComponent } from './heros/heros.component';
     HerosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
